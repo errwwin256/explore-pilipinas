dc7c974104afe5c4cde3b724dc0c71622fabe509
errwwin256 <erwingalura25@gmail.com>
2026-02-15 18:47:24 +0800

    Use BrowserRouter + keep hash links working

--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { HashRouter } from "react-router-dom"; // <-- change here
+import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <HashRouter>
-    {" "}
-    {/* <-- change here */}
+  <BrowserRouter>
     <AuthProvider>
       <App />
     </AuthProvider>
-  </HashRouter>,
+  </BrowserRouter>,
 );
